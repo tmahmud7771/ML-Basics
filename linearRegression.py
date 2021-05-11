f596a2960119b4564829ab551c5c4bd864f32995
Tausif Mahmud <tmahmud44@gmail.com>
2021-05-12 02:12:54 +0600

    comment edited

--- linearRegression.py
+++ linearRegression.py
@@ -8,7 +8,7 @@ import random as r
 # f
 # c=>f equ is f=1.8*x + 32
 
-x = list(range(0,10)) #C  #print with list method as see what happens 
+x = list(range(0,10)) #C  #print without list method as see what happens 
 y = [1.8*F +32 + r.randint(-5,5) for F in x] #F #list comprehension
 
 plt.plot(x,y,'.-r') 
